b77c6eaa00b5cf53ce79d6a179d40d0e3b440554
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-09-05 14:25:33 +0200

    Added button to log input value

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <input />
+      <button onClick={() => console.log("input value")}>Click Me</button>
       <LogsTable />
     </div>
   );
