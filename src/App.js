77b8c379f240666ec27a208c76f47fe9b3031042
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-09-05 18:38:53 +0200

    Infinite scroll for smooth filtering

--- src/App.js
+++ src/App.js
@@ -5,32 +5,55 @@ import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import logs from "./logs.json";
-import InputAndButton from "./InputAndButton";
 
-const LogsTable = ({ logs }) => {
-  return logs.map((el) => {
-    return (
-      <Accordion key={el._id} className={"logrow"}>
-        <AccordionSummary>
-          <pre>
-            <span className={el.severity}>{el.severity + " "}</span>
-            {el.timestamp}
-          </pre>
-        </AccordionSummary>
-        <AccordionDetails className={"content"}>{el.content}</AccordionDetails>
-      </Accordion>
-    );
-  });
-};
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
   const [inputFilter, setinputFilter] = useState("");
+  const [visibleLogs, setVisibleLogs] = useState(logs.slice(0, 20));
+  const fetchData = () => {
+    console.log("calling fetch data");
+    setVisibleLogs(
+      logs
+        .filter((el) => el.timestamp.includes(inputFilter))
+        .slice(0, visibleLogs.length + 20)
+    );
+  };
+
   return (
     <div className="App">
-      <InputAndButton value={inputFilter} handleChange={setinputFilter} />
-      <LogsTable
-        logs={logs.filter((el) => el.timestamp.includes(inputFilter))}
+      <input
+        value={inputFilter}
+        onChange={(e) => {
+          setinputFilter(e.target.value);
+          setVisibleLogs(
+            logs
+              .filter((el) => el.timestamp.includes(e.target.value))
+              .slice(0, 20)
+          );
+        }}
       />
+      <InfiniteScroll
+        dataLength={visibleLogs.length}
+        next={fetchData}
+        hasMore={true}
+      >
+        {visibleLogs.map((el) => {
+          return (
+            <Accordion key={el._id} className={"logrow"}>
+              <AccordionSummary>
+                <pre>
+                  <span className={el.severity}>{el.severity + " "}</span>
+                  {el.timestamp}
+                </pre>
+              </AccordionSummary>
+              <AccordionDetails className={"content"}>
+                {el.content}
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
